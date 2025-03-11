8962e8dd4067581ecb2b2646865d4e2d283d6a67
이우림 (멀티클라이언트엔지니어링) <woorimlee@i-nara.co.kr>
2025-03-11 13:13:33 +0900

    admin file updates

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,93 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Article,
+    CuisineType,
+    Restaurant,
+    RestaurantCategory,
+    RestaurantImage,
+    RestaurantMenu,
+    Review,
+    ReviewImage,
+    SocialChannel,
+    Tag,
+)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "show_at_index",
+        "is_published",
+        "created_at",
+        "modified_at",
+    ]
+    fields = ["title", "preview_image", "content", "show_at_index", "is_published"]
+    search_fields = ["title"]
+    list_filter = ["show_at_index", "is_published"]
+    date_hierarchy = "created_at"
+    actions = ["make_published"]
+
+    @admin.action(description="선택된 칼럼을 공개상태로 변경합니다")
+    def make_published(self, queryset, request):
+        queryset.update(is_published=True)
+
+
+class TagInline(admin.TabularInline):
+    model = Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    fields = ["name"]
+    search_fields = ["name"]
+
+
+class RestaurantMenuInline(admin.TabularInline):
+    model = RestaurantMenu
+    extra = 1
+
+
+class RestaurantImageInline(admin.TabularInline):
+    model = RestaurantImage
+    extra = 1
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ["name", "branch_name", "is_closed", "phone", "rating"]
+    fields = [
+        "name",
+        "branch_name",
+        "category",
+        "is_closed",
+        "phone",
+        "latitude",
+        "longitude",
+        "tags",
+    ]
+    readonly_fileds = ["rating", "rating_count"]
+    search_fields = ["name", "branch_name"]
+    list_filter = ["tags"]
+    autocomplete_fields = ["tags"]
+    inlines = [RestaurantMenuInline, RestaurantImageInline]
+
+
+@admin.register(RestaurantCategory)
+class RestaurantCategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    fields = ["cuisine_type", "name"]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["id", "restaurant_name", "author", "rating", "content_partial"]
+
+
+@admin.register(SocialChannel)
+class SocialChannelAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    fields = ["name"]
